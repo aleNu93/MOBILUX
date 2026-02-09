079666b7ce4dc98b2c3fc1598d13cfb3d94895e3
Javier Núñez Sanchez <jans8893@gmail.com>
2026-02-09 02:35:21 -0600

    Add files via upload

---
+++ DashboardPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿namespace MOBILUX.Pages;
+
+public partial class DashboardPage : ContentPage
+{
+    private bool _menuExpanded;
+
+    public DashboardPage()
+    {
+        InitializeComponent();
+        _menuExpanded = false;
+        MenuItems.IsVisible = false;
+        MenuArrow.Rotation = 0;
+    }
+
+    private void OnToggleMenuTapped(object sender, EventArgs e)
+    {
+        _menuExpanded = !_menuExpanded;
+        MenuItems.IsVisible = _menuExpanded;
+        MenuArrow.Rotation = _menuExpanded ? 180 : 0;
+    }
+
+    private async void OnPurchasesListClicked(object sender, EventArgs e)
+        => await Shell.Current.GoToAsync("PurchaseListPage");
+
+    private async void OnPurchaseDetailClicked(object sender, EventArgs e)
+        => await Shell.Current.GoToAsync("PurchaseDetailPage");
+
+    private async void OnPurchaseRegisterClicked(object sender, EventArgs e)
+        => await Shell.Current.GoToAsync("PurchaseRegisterPage");
+
+    private async void OnPaymentCreateClicked(object sender, EventArgs e)
+        => await Shell.Current.GoToAsync("PaymentCreatePage");
+
+    private async void OnProfileClicked(object sender, EventArgs e)
+        => await Shell.Current.GoToAsync("ProfilePage");
+
+    private async void OnHelpClicked(object sender, EventArgs e)
+        => await Shell.Current.GoToAsync("HelpPage");
+}
---
+++ LoginPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace MOBILUX.Pages;
+
+public partial class LoginPage : ContentPage
+{
+    public LoginPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnLoginClicked(object sender, EventArgs e)
+    {
+        // Frontend only
+        await DisplayAlert("Login", "Frontend only (no backend).", "OK");
+    }
+
+    private async void OnRecoverTapped(object sender, TappedEventArgs e)
+    {
+        await Shell.Current.GoToAsync("recover");
+    }
+
+    private async void OnRegisterTapped(object sender, TappedEventArgs e)
+    {
+        await Shell.Current.GoToAsync("register");
+    }
+}
