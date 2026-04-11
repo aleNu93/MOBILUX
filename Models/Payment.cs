6b9830438f30b4c9f60618afaf6c9245c5193656
Javier Núñez Sanchez <jans8893@gmail.com>
2026-04-11 02:04:24 -0600

    Delete Models directory

--- Models/Payment.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MOBILUX.Models;
-
-public class Payment
-{
-    public string Id { get; set; } = "";
-    public string PurchaseId { get; set; } = "";
-    public decimal Amount { get; set; }
-    public DateTime PaymentDate { get; set; }
-}
